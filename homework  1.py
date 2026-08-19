2e6678b4c376f8acafa405abb2b56625179bc16d
adedibubukola-daniel <adedibubukola@gmail.com>
2026-08-19 13:37:24 +0100

    Add files via upload

---
+++ homework  1.py
@@ -0,0 +1,15 @@
+print("my daily routine")
+# step 1:waking up
+print("i wake up at 6:30 am")
+# step 2:going to school
+print("i go to school by 7 am")
+# step 3 :playing after school
+print("i play with my freinds after school")
+# step 4 :watching television
+print("i watch television after playing with my freinds")
+# step 5 :studying at night
+print("i study for 2 hours at night")
+# calculating how many hours i read each week
+print("total study hours in a week")
+# total study hours in a week
+print("2*7=14 hours in a week") 
