ff01351deaa751bc23b020001ac90fcc0d8db2c6
adedibubukola-daniel <adedibubukola@gmail.com>
2026-08-27 12:52:04 +0100

    Add files via upload

---
+++ farm harvest calculator.py
@@ -0,0 +1,16 @@
+feild1=100 # type: ignore
+feild2=120
+feild3=130
+feild4=140
+feild5=150
+total=feild1+feild2+feild3+feild4+feild5 
+average=total/5
+print("total",total)
+print("average",average)
+pricepackagekg=15
+totalprice=total*pricepackagekg
+print("total price",totalprice)
+bags=total//25
+leftovers=total%25
+print("bags",bags)
+print("leftovers",leftovers)
