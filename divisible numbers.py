45df453d83a2e7d26cac9f7aa7cbc66dfa329622
adedibubukola-daniel <adedibubukola@gmail.com>
2026-09-03 12:39:44 +0100

    Add files via upload

---
+++ divisible numbers.py
@@ -0,0 +1,8 @@
+print("enter a number(numerator)")
+numerator=int(input())
+print("enter a denominator")
+denominator=int(input())
+if numerator%denominator==0:
+    print("number is divisible")
+else:
+    print ("number is not divisible")
---
+++ operator predence.py
@@ -0,0 +1,6 @@
+v=5
+w=8
+x=10
+y=7
+z=(v+w)*x/y
+print("the result is",z)
